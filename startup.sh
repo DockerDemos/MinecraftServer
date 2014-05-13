f4f2414f7bde855f7e0a26012ca2754f6d010216
Chris Collins <collins.christopher@gmail.com>
2014-05-13 12:44:13 -0400

    added msm update, and fixed version detection

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+yes | /usr/local/bin/msm update
+
 /usr/local/bin/msm jargroup create minecraft https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar
 /usr/local/bin/msm server create $(hostname)
 /usr/local/bin/msm $(hostname) jar minecraft
 
-VERSION=$(/bin/grep minecraft\/ /srv/minecraft-server-manager/versioning/versions.txt | sort |tail -n1)
-
+VERSION="minecraft/$(ls /opt/msm/versioning | sort |tail -n1 |sed 's/.sh//')"
 /bin/echo "msm-version=$VERSION" >> /opt/msm/servers/$(hostname)/server.properties
 
 /usr/local/bin/msm $(hostname) start
